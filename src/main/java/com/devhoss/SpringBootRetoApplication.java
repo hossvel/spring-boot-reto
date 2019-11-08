2fcf7bfcda372d83b329f98f9ead419b995c7d63
devhoss <velasco2288@gmail.com>
2019-11-07 22:31:42 -0500

    primera subida

---
+++ src/main/java/com/devhoss/SpringBootRetoApplication.java
@@ -0,0 +1,13 @@
+package com.devhoss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRetoApplication.class, args);
+	}
+
+}
