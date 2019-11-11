9c8b04c262b4c1d80b1b65ec0549b688eda28308
devhoss <velasco2288@gmail.com>
2019-11-10 22:15:38 -0500

    agrega config

--- src/main/java/com/devhoss/SpringBootRetoApplication.java
+++ src/main/java/com/devhoss/SpringBootRetoApplication.java
@@ -1,36 +1,11 @@
 package com.devhoss;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-
 
 @SpringBootApplication
-@EnableSwagger2
 public class SpringBootRetoApplication {
 
-	  @Bean
-	    public Docket swagger() {
-	        return new Docket(DocumentationType.SWAGGER_2)
-	                .select()
-	                .apis(RequestHandlerSelectors.basePackage("com.devhoss"))
-	                .paths(PathSelectors.any())
-	                .build();
-	    }
-	   
-	  @Bean
-	   public ModelMapper modelMapper() {
-	      ModelMapper modelMapper = new ModelMapper();
-	      return modelMapper;
-	   }
-	  
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootRetoApplication.class, args);
 	}
